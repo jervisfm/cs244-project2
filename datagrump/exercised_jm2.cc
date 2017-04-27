b081e608eaa24a5061c559432eb7844971311940
Jervis Muindi <jervismuindi@yahoo.co.uk>
2017-04-26 17:08:09 -0700

    Add a new fork of exercise d to experiment with non-bbr approaches

---
+++ datagrump/exercised_jm2.cc
@@ -0,0 +1,250 @@
+#include <iostream>
+#include <sstream>
+#include <cassert>
+#include <limits>
+#include <cmath>
+
+#include "controller.hh"
+#include "timestamp.hh"
+#include "exercised_jm2.hh"
+
+
+using namespace std;
+
+// Size of the UDP packets we're sending.
+static const int PACKET_SIZE_BYTES = 1472;
+
+
+
+/* Default constructor */
+ExDJM2Controller::ExDJM2Controller( const bool debug )
+  : Controller::Controller( debug ),
+    bbr_state_(BBR_STATE::STARTUP),
+    inflight_packets_(0),
+    cwnd_(1),
+    cwnd_gain_(0.8),
+    pacing_gain_(0.9),
+    num_bytes_sent_(0),
+    last_sequence_number_acked_(0),
+    rtt_samples_(),
+    time_to_data_map_()
+          
+{
+  cerr << "Exercise D Jervis Controller" << endl;
+}
+
+/* Get current window size, in datagrams */
+unsigned int ExDJM2Controller::window_size( void )
+{
+  int window_size = 0;
+
+    // BBR says that the window size should be some fraction of the BDP as modulated
+    // by the cwnd gain.
+    window_size = bandwidth_delay_product() * cwnd_gain_;
+
+  
+  // BBR recommends always have a min window size of 4 to keep things going smoothly.
+  window_size = std::max(4, window_size);
+  
+  // window_size = 1;
+
+  //  debug_printf(INFO, "At time %d, window size is %d", timestamp_ms(), window_size);
+  debug_printf(VERBOSE, "At time %d, window size is %d", timestamp_ms(), window_size);
+
+  return window_size;
+}
+
+
+/* A datagram was sent */
+void ExDJM2Controller::datagram_was_sent(  const uint64_t sequence_number, /* of the sent datagram */
+                                        const uint64_t send_timestamp, /* in milliseconds */
+                                        bool on_timeout )
+{
+  /* Default: take no action */
+  debug_printf(VERBOSE, "At time %d sent datagram %d. Was timeout?%b", send_timestamp, sequence_number, on_timeout);
+  ++inflight_packets_;
+}
+
+double ExDJM2Controller::average_rtt() {
+  double total = 0;
+  int count = 0;
+  for (double value : rtt_samples_) {
+    total += value;
+    ++count;
+  }
+  return total/count;
+}
+
+double ExDJM2Controller::sliding_min_rtt(int num_samples) {
+
+  double current_min_rtt = std::numeric_limits<double>::max();
+  int count = 1;
+  for(auto it = rtt_samples_.rbegin(); it != rtt_samples_.rend(); ++it) {
+    if (count >= num_samples) {
+      break;
+    }
+    double new_sample = *it;
+    current_min_rtt = std::min(new_sample, current_min_rtt);
+    ++count;
+  }
+  return current_min_rtt;
+}
+
+std::vector<double> ExDJM2Controller::delivery_rates() {
+  std::vector<double> rates;
+  // Loop over all the time -> num data sent samples in order and compute a vectory
+  // of delivery rates so far.
+  int count = 0;
+  std::pair<int, double> previous_entry;
+  for (const auto& entry : time_to_data_map_) {
+    ++count;
+    if (count == 1) {
+      previous_entry = entry;
+      // We need two entries to compute a rate.
+      continue;
+    }
+
+    // Pull out the needed data pieces from the two entries.
+    int entry_time_sent = entry.first;
+    double entry_data_sent = entry.second;
+
+    int prev_entry_time_sent = previous_entry.first;
+    double prev_entry_data_sent = previous_entry.second;
+
+    // Compute intermediate deltas
+    double delta_time = entry_time_sent - prev_entry_time_sent;
+    double delta_data_sent = entry_data_sent - prev_entry_data_sent;
+
+    // TODO: Remove debug assertion.
+    assert(delta_time > 0);
+
+    // Compute instantenous delivery rate from the deltas.
+    double delivery_rate = delta_data_sent / delta_time;
+    rates.emplace_back(delivery_rate);
+
+    previous_entry = entry;
+  }
+  return rates;
+}
+
+double ExDJM2Controller::bandwidth_delay_product() {
+  return sliding_min_rtt() * sliding_max_bandwidth();
+}
+
+double ExDJM2Controller::sliding_max_bandwidth(int num_samples) {
+  std::vector<double> rates = delivery_rates();
+  int count = 1;
+  double current_max = 0;
+  for (auto it = rates.rbegin(); it != rates.rend(); ++it) {
+    if (count > num_samples) {
+      break;
+    }
+    double new_sample = *it;
+    current_max = std::max(new_sample, current_max);
+    ++count;
+  }
+  return current_max;
+}
+
+void ExDJM2Controller::test_delivery_rates() { 
+  time_to_data_map_ = {{1,10}, {2,20}, {3, 30}, {4,50}, {5,90}};
+  stringstream output;
+  for (const auto& rate : delivery_rates()) {
+    output << rate << ", ";
+  }
+  debug_printf(INFO, "Rates: %s", output.str().c_str());
+}
+
+bool ExDJM2Controller::delivery_rate_increased() {
+  std::vector<double> rates = delivery_rates();
+  if (rates.size() <= 1) {
+    // Be optimistic and assume rate is increasing if we don't have enough data.
+    return true;
+  }
+
+  double current_value = rates[rates.size() - 1];
+  double prior_value =  rates[rates.size() - 2];
+  double change_magnitude = (current_value / prior_value);
+  double change_threshold = 1.20;
+  if (change_magnitude >= change_threshold) {
+    return true;
+  } else {
+    return false;
+  }  
+}
+
+double ExDJM2Controller::inflight_bdp() {
+  double result = (PACKET_SIZE_BYTES / sliding_min_rtt()) * inflight_packets_;
+  return result;
+}
+
+/* An ack was received */
+void ExDJM2Controller::ack_received( const uint64_t sequence_number_acked,
+                                  const uint64_t send_timestamp_acked,
+                                  const uint64_t recv_timestamp_acked,
+                                  const uint64_t timestamp_ack_received )
+{
+  --inflight_packets_;
+
+  // Gather data for RTT estimates.
+  double rtt_ms = timestamp_ack_received - send_timestamp_acked;
+  rtt_samples_.emplace_back(rtt_ms);
+  double average_rtt_ms = average_rtt();
+
+  // Update stats for delivery rate.
+  // TODO: Do we need to worry about out of order UDP packet delivery ? 
+  if (sequence_number_acked > last_sequence_number_acked_) {
+    // Got a new data point.
+    last_sequence_number_acked_ = sequence_number_acked;
+  }
+  num_bytes_sent_ += PACKET_SIZE_BYTES;
+  time_to_data_map_.emplace(timestamp_ack_received, num_bytes_sent_);
+  
+  debug_printf(VERBOSE, "At time=%d received ack for datagram=%d. Sent: %d. Receipt (recv's clock): %d  RTT(ms): %.1f Running Avg RTT(ms): %.1f",
+               timestamp_ack_received, sequence_number_acked, send_timestamp_acked,
+               recv_timestamp_acked, rtt_ms, average_rtt_ms);
+  debug_printf(INFO, "Sliding Window Min RTT(ms): %.1f Sliding Window Max Bandwidth: %.1f", sliding_min_rtt(), sliding_max_bandwidth());
+
+
+  // In start up mode, grow the cwnd exponetially.
+  debug_printf(INFO, "Mode: %d", bbr_state_);
+  if (mode_startup()) {
+    debug_printf(INFO, "Start up mode");
+    if (delivery_rate_increased()) {
+      cwnd_gain_ = HIGH_GAIN;
+      pacing_gain_ = HIGH_GAIN;
+    } else {
+      switch_to_mode_drain();
+    } 
+  } else if (mode_drain()) {
+    debug_printf(INFO, "Drain mode");
+    double target_bdp = bandwidth_delay_product();
+    if (inflight_bdp() <= target_bdp) {
+      // Switch to cruise mode aka Probe_BW
+      switch_to_mode_probe_bw();
+    } else {
+      debug_printf(INFO, "inflight pkts: %d target_bdp(bytes): %.f inflight_bdp: %.f DIFF: %.f",
+                   inflight_packets_, target_bdp, inflight_bdp(), (inflight_bdp() - target_bdp)); 
+    }
+  } else if (mode_probe_bw()) {
+    // TODO
+    // Implement gain cycling for Probe BW.
+    debug_printf(INFO, "Probe BW  mode");
+
+  }
+
+  
+  
+}
+
+/* How long to wait (in milliseconds) if there are no acks
+   before sending one more datagram */
+unsigned int ExDJM2Controller::timeout_ms( void )
+{
+  // This is equivalent to the timer callback delay in the BBR paper.
+  // It's set to: PktSize / (pacing_gain * BtlBw)
+  int timeout = PACKET_SIZE_BYTES / (pacing_gain_ * sliding_max_bandwidth());
+  timeout = std::max(timeout, 1);
+  debug_printf(VERBOSE, "Waiting for timeout (ms): %d", timeout);
+  return timeout;
+}
---
+++ datagrump/exercised_jm2.hh
@@ -0,0 +1,117 @@
+#ifndef EXERCISE_D_JM2_CONTROLLER_HH
+#define EXERCISE_D_JM2_CONTROLLER_HH
+
+#include <cmath>
+#include <vector>
+#include <map>
+
+static const double HIGH_GAIN = 2.0 / log(2);
+
+// Exercise D controller for Jervis' implementation.
+// A custom controller implemementation that's forked off an earlier attempt at BBR.
+// Main idea is to try explore effect of just increasing Window whilst RRT is constant.
+class ExDJM2Controller : public Controller {
+private:
+    enum BBR_STATE {
+     STARTUP,
+     DRAIN,
+     PROBE_BW,
+     PROBE_RTT,
+    };
+    // State of bbr algorithm
+    BBR_STATE bbr_state_;
+    // Number of packets outstanding in the network.
+    int inflight_packets_;
+    // Value to control cwnd directly in startup mode.
+    double cwnd_;
+    // Factor to fudge BDP by.
+    double cwnd_gain_;
+    // Factor to fudge the delay by.
+    double pacing_gain_;
+    // Total number data bytes sent.
+    double num_bytes_sent_;
+    // The greatest sequence number successfully acked so far.
+    uint64_t last_sequence_number_acked_;
+    // Keeps track of all RTT samples we have seen so far.
+    std::vector<double> rtt_samples_;
+    // Map of time -> how much data sent at that time.
+    std::map<int, double> time_to_data_map_;
+
+  // Returns the current average rtt
+  double average_rtt();
+
+  // Computes the delivery rate from the collected time_to_data_map_;
+  // This is amount of data sent / time. The rates computed are in
+  // time order.
+  std::vector<double> delivery_rates();
+
+  // Returns true if we saw substantial gains in delivery rate.
+  bool delivery_rate_increased();
+  
+  // Returns the minimum rtt about the past number of samples. Unit is milliseconds.
+  double sliding_min_rtt(int num_samples=10);
+
+  // Returns the maximum bandwidth over the past number of samples. Unit is bytes/msec
+  double sliding_max_bandwidth(int num_samples=10);
+  
+  // Returns the current estimate for bandwidth delay product. This is a product
+  // sliding_min_rtt() and sliding_max_bandwidth().
+  double bandwidth_delay_product();
+
+  // Computes the (estimated) inflight bandwidth delay product. Useful to know when
+  // to swutch to PROBE_BW mode.
+  double inflight_bdp() ;
+  
+  // Debugging test functions. Can be removed once we're confident in the implementation.
+  void test_delivery_rates();
+
+  inline bool mode_startup() {
+    return bbr_state_ == BBR_STATE::STARTUP;
+  }
+
+  inline bool mode_probe_bw() {
+    return bbr_state_ == BBR_STATE::PROBE_BW;
+  }
+
+  inline bool mode_probe_rtt() {
+    return bbr_state_ == BBR_STATE::PROBE_RTT;
+  }
+
+  inline bool mode_drain() {
+    return bbr_state_ == BBR_STATE::DRAIN;
+  }
+
+  inline void switch_to_mode_drain() {
+    // Switch to drain mode. Keep the original cwnd gain inplace.
+    bbr_state_ = BBR_STATE::DRAIN;
+    pacing_gain_ = 1.0/HIGH_GAIN;
+    cwnd_gain_ = HIGH_GAIN;
+  }
+
+  inline void switch_to_mode_probe_bw() {
+    bbr_state_ = BBR_STATE::PROBE_BW;
+    pacing_gain_ = 1.0;
+    cwnd_gain_ = 2;
+  }
+  
+public:
+    ExDJM2Controller( const bool debug);
+
+    unsigned int window_size() override;
+
+    void datagram_was_sent( const uint64_t sequence_number,
+                            const uint64_t send_timestamp,
+                            bool on_timeout) override ;
+
+    void ack_received( const uint64_t sequence_number_acked,
+            /* what sequence number was acknowledged */
+                       const uint64_t send_timestamp_acked,
+            /* when the acknowledged datagram was sent (sender's clock) */
+                       const uint64_t recv_timestamp_acked,
+            /* when the acknowledged datagram was received (receiver's clock)*/
+                       const uint64_t timestamp_ack_received ) override;
+
+    unsigned int timeout_ms( void ) override;
+};
+
+#endif
